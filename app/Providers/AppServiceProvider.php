34231fb30fabae5dc8a74f41f6267186eb40202f
Ezra Lazuardy <ezralucio@gmail.com>
2023-10-11 01:12:49 +0700

    feat: update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,23 +2,54 @@
 
 namespace App\Providers;
 
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Collection;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
-     * Register any application services.
+     * Register any application services
      */
     public function register(): void
     {
-        //
+        if ($this->app->isLocal()) {
+            $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
+        }
     }
 
     /**
-     * Bootstrap any application services.
+     * Bootstrap any application services
      */
     public function boot(): void
     {
-        //
+        // register macros
+        $this->registerMacros();
+
+        // unwrap "data" properties in resource object
+        JsonResource::withoutWrapping();
+    }
+
+    /**
+     * Register application macros
+     */
+    public function registerMacros(): void
+    {
+        // register paginate macro for collection
+        Collection::macro('paginate', function ($perPage = 15, $total = null, $page = null, $pageName = 'page') {
+            $page = $page ?: LengthAwarePaginator::resolveCurrentPage($pageName);
+
+            return new LengthAwarePaginator(
+                $total ? $this : $this->forPage($page, $perPage)->values(),
+                $total ?: $this->count(),
+                $perPage,
+                $page,
+                [
+                    'path' => LengthAwarePaginator::resolveCurrentPath(),
+                    'pageName' => $pageName,
+                ]
+            );
+        });
     }
 }
